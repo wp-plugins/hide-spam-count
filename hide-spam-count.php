5818a5a14241b0446c1d3f2c6cceeefb7bb2fdda
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-29 19:47:20 +0000

    update readme
    git-svn-id: https://plugins.svn.wordpress.org/hide-spam-count/trunk@282322 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hide-spam-count.php
+++ hide-spam-count.php
@@ -4,11 +4,26 @@ Plugin Name: Hide Spam Count
 Version: 1.0.1
 Description: Hide the annoying spam count from your WordPress Dashboard
 Author: scribu
-Plugin URI: http://scribu.net/wordpress/hide-spam-count
 Author URI: http://scribu.net/
+Plugin URI: http://scribu.net/wordpress/hide-spam-count
+
+
+Copyright (C) 2010 Cristi Burcă (scribu@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-add_action('admin_print_styles-index.php', 'hide_spam_count');
 function hide_spam_count() {
 ?>
 <style type="text/css">
@@ -16,9 +31,10 @@ td.b-spam, td.b-spam + td {visibility:hidden !important}
 </style>
 <?php
 }
+add_action('admin_print_styles-index.php', 'hide_spam_count');
 
-add_action('plugins_loaded', 'hide_spam_stats');
 function hide_spam_stats() {
 	remove_action('rightnow_end', 'akismet_rightnow');
 }
+add_action('plugins_loaded', 'hide_spam_stats');
 
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: scribu
 Donate link: http://scribu.net/paypal
 Tags: dashboard, admin, spam, akismet
 Requires at least: 2.5
-Tested up to: 2.9
+Tested up to: 3.0
 Stable tag: trunk
 
 Hide the annoying spam count from the Dashboard.
@@ -15,6 +15,8 @@ Whenever I visit my site's dashboard, the first thing I see is the bold red Spam
 Instead of wasting time deleting spam comments, I prefer to let Akismet do that automatically, so I don't need to see how many spam comments there are.
 
 This little plugin simply hides that part.
+
+Links: [Plugin News](http://scribu.net/wordpress/hide-spam-count) | [Author's Site](http://scribu.net)
 
 == Changelog ==
 
